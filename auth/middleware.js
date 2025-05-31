1bab1a49a319c5f7c20f06eaf0660f142893ec23
Alessio Caringella <156083157+Alecaring@users.noreply.github.com>
2025-05-31 02:43:47 +0200

    Update middleware.js

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,8 +1,1 @@
-import { isLoggedIn } from '../auth/auth.js';
 
-export function requireAuth() {
-    if (!isLoggedIn()) {
-        // alert('Devi essere loggato per accedere');
-        window.location.href = 'login.html';
-    }
-}
